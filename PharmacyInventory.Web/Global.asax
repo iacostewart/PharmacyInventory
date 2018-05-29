bc2fe9e64114ecf1590a2c64e918eebb6dd99c31
Ian Stewart <iacostewart@msn.com>
2018-05-29 19:22:09 -0400

    Add project files.

---
+++ PharmacyInventory.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PharmacyInventory.Web.MvcApplication" Language="C#" %>
---
+++ PharmacyInventory.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PharmacyInventory.Web.Startup))]
+namespace PharmacyInventory.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
